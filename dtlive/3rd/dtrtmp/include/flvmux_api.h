0a44f0713bcf24b6a0b950a47aef52e22e8b57c6
peter-s <peter_future@outlook.com>
2016-07-13 15:18:27 +0800

    update dtrtmp

--- dtlive/3rd/dtrtmp/include/flvmux_api.h
+++ dtlive/3rd/dtrtmp/include/flvmux_api.h
@@ -2,7 +2,7 @@
  * =====================================================================================
  *
  *    Filename   :  flv_mux.h
- *    Description:  
+ *    Description:
  *    Version    :  1.0
  *    Created    :  2016年06月30日 13时15分35秒
  *    Revision   :  none
@@ -23,21 +23,36 @@
 
 struct flvmux_packet {
     uint8_t *data;
-    int size;
+    uint32_t size;
     int64_t pts;
     int64_t dts;
+
+    int type; // 0-v 1-a
 };
 
 struct flvmux_para {
-    int hav_audio;
+    int has_audio;
     int has_video;
     int afmt;
     int vfmt;
 };
 
+#define AAC_ADTS_HEADER_SIZE 7
+#define FLV_TAG_HEAD_LEN 11
+#define FLV_PRE_TAG_LEN 4
+struct AudioSpecificConfig {
+    uint8_t audio_object_type;
+    uint8_t sample_frequency_index;
+    uint8_t channel_configuration;
+};
+
 struct flvmux_context {
     struct flvmux_para para;
-    char header[1024];
+    // audio
+    int audio_config_ok;
+    struct AudioSpecificConfig config;
+    // video 
+    uint8_t header[1024];
     int header_size;
     int video_config_ok;
 };
